25e1e14392e137a4b2bd6239abdf95eb0476f82c
Damian Sniezek <damian.sniezek@bitbar.com>
2019-11-07 09:35:43 +0100

    Version 2.85, pass ADB version to device session

--- src/main/java/com/bitbar/remotedevice/api/APIClientManager.java
+++ src/main/java/com/bitbar/remotedevice/api/APIClientManager.java
@@ -5,30 +5,29 @@ import com.bitbar.remotedevice.errors.RequiredParameterIsEmptyException;
 import com.testdroid.api.*;
 import com.testdroid.api.dto.Context;
 import com.testdroid.api.dto.MappingKey;
-import com.testdroid.api.dto.Operand;
 import com.testdroid.api.filter.FilterEntry;
 import com.testdroid.api.filter.ListStringFilterEntry;
 import com.testdroid.api.filter.NumberFilterEntry;
-import com.testdroid.api.filter.StringFilterEntry;
-import com.testdroid.api.model.*;
+import com.testdroid.api.model.APICloudInfo;
+import com.testdroid.api.model.APIDevice;
+import com.testdroid.api.model.APIDeviceSession;
+import com.testdroid.api.model.APIDeviceSessionConfig;
 import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 
 import static com.bitbar.remotedevice.StaticParameters.DEVICE_SESSIONS_URI;
-import static com.bitbar.remotedevice.StaticParameters.RELEASE_DS_URI;
-import static com.testdroid.api.dto.MappingKey.LABEL_IDS_ARR;
+import static com.testdroid.api.dto.MappingKey.*;
+import static com.testdroid.api.dto.Operand.EQ;
+import static com.testdroid.api.dto.Operand.IN;
+import static com.testdroid.api.model.APIDevice.OsType.ANDROID;
+import static com.testdroid.api.model.APIDevice.OsType.IOS;
+import static com.testdroid.api.model.APIDeviceSession.Type.REMOTE;
 
 public class APIClientManager {
 
-    private static final String DEVICE_MODEL_ID_PARAM = "deviceModelId";
-
     private static final String INFO_URI = "/info";
-
-    private static final String TYPE_PARAM = "type";
-
-    private static final String TYPE_PARAM_VALUE = "REMOTE";
 
     private APIClient apiClient;
 
@@ -45,27 +44,14 @@ public class APIClientManager {
         return new APIKeyClient(cloudUrl, apiKey);
     }
 
-    private Optional<Long> getRemoteSessionsLabelId() throws APIException {
-        Optional<Long> result = Optional.empty();
-        Context<APILabelGroup> ctx = new Context<>(APILabelGroup.class);
-        ctx.addFilter(new StringFilterEntry(MappingKey.NAME, Operand.EQ, "supported-frameworks"));
-        List<APILabelGroup> labelGroups = apiClient.getLabelGroups(ctx).getEntity().getData();
-        if (labelGroups.size() > 0) {
-            Context<APIDeviceProperty> lCtx = new Context<>(APIDeviceProperty.class);
-            lCtx.addFilter(new StringFilterEntry(MappingKey.NAME, Operand.EQ, "remote-session"));
-            result = labelGroups.get(0).getDevicePropertiesResource(lCtx).getEntity().getData()
-                    .stream().findFirst().map(APIEntity::getId);
-        }
-        return result;
-    }
-
     public List<APIDevice> getSupportedDevices() throws APIException {
         Context<APIDevice> ctx = new Context<>(APIDevice.class);
-        ctx.getFilters().add(new ListStringFilterEntry(MappingKey.OS_TYPE, Operand.IN,
-                Arrays.asList(APIDevice.OsType.ANDROID.getDisplayName(), APIDevice.OsType.IOS.getDisplayName())));
-        getRemoteSessionsLabelId().ifPresent(val -> ctx.setExtraParams(
-                new HashSetValuedHashMap<>(Collections.singletonMap(LABEL_IDS_ARR, val))
-        ));
+        ctx.getFilters().add(new ListStringFilterEntry(OS_TYPE, IN,
+                Arrays.asList(ANDROID.getDisplayName(), IOS.getDisplayName())));
+        apiClient.findDevicePropertyInLabelGroup("supported-frameworks", "remote-session").
+                ifPresent(val -> ctx.setExtraParams(
+                        new HashSetValuedHashMap<>(Collections.singletonMap(LABEL_IDS_ARR, val.getId()))
+                ));
         ctx.setLimit(0);
         List<APISort.SortItem> sortItems = new ArrayList<>();
         sortItems.add(new APISort.SortItem(MappingKey.DISPLAY_NAME, APISort.Type.ASC));
@@ -76,7 +62,7 @@ public class APIClientManager {
     public APIDevice getDevice(Long id) throws APIException {
         Context<APIDevice> ctx = new Context<>(APIDevice.class);
         List<FilterEntry> filters = ctx.getFilters();
-        filters.add(new NumberFilterEntry(MappingKey.ID, Operand.EQ, id));
+        filters.add(new NumberFilterEntry(ID, EQ, id));
         APIList<APIDevice> devices = apiClient.getDevices(ctx).getEntity();
         if (devices.isEmpty()) {
             throw new APIException(String.format("Could not find device with id %d", id));
@@ -84,22 +70,20 @@ public class APIClientManager {
         return devices.get(0);
     }
 
-    public APIDeviceSession createDeviceSession(Long deviceModelId)
+    public APIDeviceSession createDeviceSession(Long deviceModelId, String adbVersion)
             throws APIException, RequiredParameterIsEmptyException {
         if (deviceModelId == null) {
             throw new RequiredParameterIsEmptyException(CommandLineParameter.DEVICE_MODEL_ID);
         }
-
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put(DEVICE_MODEL_ID_PARAM, deviceModelId);
-        parameters.put(TYPE_PARAM, TYPE_PARAM_VALUE);
-
-        APIDeviceSession deviceSession = apiClient.post(DEVICE_SESSIONS_URI, parameters, APIDeviceSession.class);
-        return deviceSession;
+        APIDeviceSessionConfig config = new APIDeviceSessionConfig();
+        config.setDeviceModelId(deviceModelId);
+        config.setType(REMOTE);
+        Optional.ofNullable(adbVersion).ifPresent(config::setAdbVersion);
+        return apiClient.post(DEVICE_SESSIONS_URI, config, APIDeviceSession.class);
     }
 
     public void releaseDeviceSession(APIDeviceSession deviceSession) throws APIException {
-        apiClient.post(String.format(RELEASE_DS_URI, deviceSession.getId()), null, APIDeviceSession.class);
+        deviceSession.release();
     }
 
     public String getBackendUrl() throws APIException {
